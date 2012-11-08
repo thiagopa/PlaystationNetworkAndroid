6c699c1bbec2a613a9d4d6ec31eeb54f0d11e520
Thiago Pagonha <thi.pag@gmail.com>
2012-11-08 01:56:27 -0200

    Implementado Tarefa que verifica se um amigo não está mais online e o marca como offline

--- src/br/com/thiagopagonha/psnapi/FriendActivity.java
+++ src/br/com/thiagopagonha/psnapi/FriendActivity.java
@@ -1,16 +1,26 @@
 package br.com.thiagopagonha.psnapi;
 
 import static br.com.thiagopagonha.psnapi.utils.CommonUtilities.REFRESH_FRIENDS;
+import static br.com.thiagopagonha.psnapi.utils.CommonUtilities.TAG;
+
+import java.util.GregorianCalendar;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -20,6 +30,8 @@ import br.com.thiagopagonha.psnapi.utils.ImageCache;
 
 public class FriendActivity extends Activity {
 
+	private ScheduledExecutorService scheduleTaskExecutor;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,8 +39,65 @@ public class FriendActivity extends Activity {
 		registerReceiver(refreshFriends, new IntentFilter(REFRESH_FRIENDS));
 		// -- Desenha a Tela
 		renderView();
+		// -- Cria o executor pra verificar o status dos amigos
+		scheduleTaskExecutor= Executors.newScheduledThreadPool(5);
+		// This schedule a task to run every 15 minutes:
+	    // -- Lógica que verifica se o amigo está ou não mais online
+		scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+	      public void run() {
+	        
+	    	FriendsDBHelper friendsDBHelper = new FriendsDBHelper(getApplicationContext());
+	    	  
+	    	Log.d(TAG, "Friend is Offline Task");  
+	    	long now =  System.currentTimeMillis();
+	    	boolean updateView = false;  
+	    	for (Friend friend : friendsDBHelper.getFriends()) {
+				
+	    		long lastSeen = friend.getUpdated().getTime();
+
+	    		long diff = now - lastSeen;
+	    		
+	    		boolean isMoreThan15Minutes = diff > 900000l;
+	    		
+	    		Log.d(TAG, "Now " + now);
+	    		Log.d(TAG, "LastSeen " + lastSeen);
+	    		Log.d(TAG, "Difference " + diff);
+	    		Log.d(TAG, "IsMore? " + isMoreThan15Minutes);
+	    		
+	    		// -- Se o intervalo entre as datas for maior que 15 minutos em milisegundos XD
+	    		if(isMoreThan15Minutes) {
+	    			Log.d(TAG, friend.getPsnId() + " is Offline, updating info");  
+	    			friendsDBHelper.saveFriend(friend.getPsnId(), "Offline", friend.getAvatarSmall());
+	    			
+	    			updateView = true;
+	    		}
+	    		
+			}
+	    	
+	    	if(updateView) {
+		        // If you need update UI, simply do this:
+		        runOnUiThread(new Runnable() {
+		          public void run() {
+		            // update your UI component here.
+		            renderView();
+		          }
+		        });
+	    	}
+	    	
+	    	friendsDBHelper.close();
+	    	
+	      }
+	    }, 0, 1, TimeUnit.MINUTES);
 	}
 
+	// -- Necessário, mesmo :P
+	protected void onDestroy() {
+		scheduleTaskExecutor.shutdown();
+		unregisterReceiver(refreshFriends);
+		super.onDestroy();
+		
+	}
+	
 	/**
 	 * Receiver que recebe a chamada sempre que chega uma mensagem nova
 	 */
@@ -47,26 +116,41 @@ public class FriendActivity extends Activity {
 	private void renderView() {
 		
 		TableLayout table = new TableLayout(this);  
-		table.setStretchAllColumns(true);  
-		table.setShrinkAllColumns(true);  
+
+		// -- Ajusta o tamanho das colunas, preservando a primeira que é a do avatar
+		table.setColumnStretchable(1, true);
+		table.setColumnStretchable(2, true);
 		
-		 
-		 for (Friend friend : new FriendsDBHelper(getApplicationContext()).getFriends()) {
+		table.setColumnShrinkable(1, true);
+		table.setColumnShrinkable(2, true);
+		
+		FriendsDBHelper friendsDBHelper = new FriendsDBHelper(getApplicationContext());
+		
+		 for (Friend friend : friendsDBHelper.getFriends()) {
 			 TableRow row = new TableRow(this);
-			 
+			 // -- Mantém todas as colunas com conteúdo alinhado no centro
 			 row.setGravity(Gravity.CENTER_VERTICAL);  
 			 
 			 ImageView avatar = new ImageView(this);
+			 // -- Pulo do gato, aqui é pego a imagem do cache ou feito o download
 			 avatar.setImageBitmap( ImageCache.getImage(friend.getAvatarSmall()) );
+			 // -- Não deixa a imagem deformar
+			 avatar.setScaleType(ScaleType.CENTER);
 			 
+			 // -- Nome do usuário da PSN
 			 TextView psnId = new TextView(this);
 			 psnId.setText(friend.getPsnId());
 			 psnId.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18); 
 			 psnId.setTypeface(Typeface.SERIF, Typeface.BOLD);
 			 
+			 // -- O jogo que está jogando
 			 TextView playing = new TextView(this);
 			 playing.setText(friend.getPlaying());
-
+			 playing.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16); 
+			 playing.setTypeface(Typeface.SERIF, Typeface.BOLD);
+			 playing.setTextColor(Color.BLUE);
+			 
+			 // -- Adiciona as views na tabela
 			 row.addView(avatar);
 			 row.addView(psnId);
 			 row.addView(playing);
@@ -74,7 +158,8 @@ public class FriendActivity extends Activity {
 			 table.addView(row);
 		}
 		
+		friendsDBHelper.close();
+		 
 		setContentView(table);
 	}
-	
 }
