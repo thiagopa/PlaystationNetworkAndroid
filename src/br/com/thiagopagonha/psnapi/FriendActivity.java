c36946e5ba7b00330c7edbb865e7a277286e2a9c
Thiago Pagonha <thi.pag@gmail.com>
2012-11-08 00:18:53 -0200

    Ajustando Layout da Tela

--- src/br/com/thiagopagonha/psnapi/FriendActivity.java
+++ src/br/com/thiagopagonha/psnapi/FriendActivity.java
@@ -1,12 +1,15 @@
 package br.com.thiagopagonha.psnapi;
 
-import static br.com.thiagopagonha.psnapi.utils.CommonUtilities.*;
+import static br.com.thiagopagonha.psnapi.utils.CommonUtilities.REFRESH_FRIENDS;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -51,19 +54,23 @@ public class FriendActivity extends Activity {
 		 for (Friend friend : new FriendsDBHelper(getApplicationContext()).getFriends()) {
 			 TableRow row = new TableRow(this);
 			 
+			 row.setGravity(Gravity.CENTER_VERTICAL);  
+			 
 			 ImageView avatar = new ImageView(this);
 			 avatar.setImageBitmap( ImageCache.getImage(friend.getAvatarSmall()) );
 			 
 			 TextView psnId = new TextView(this);
 			 psnId.setText(friend.getPsnId());
-
+			 psnId.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18); 
+			 psnId.setTypeface(Typeface.SERIF, Typeface.BOLD);
+			 
 			 TextView playing = new TextView(this);
 			 playing.setText(friend.getPlaying());
-			 
 
 			 row.addView(avatar);
 			 row.addView(psnId);
 			 row.addView(playing);
+			 
 			 table.addView(row);
 		}
 		
